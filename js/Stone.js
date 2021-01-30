df9bed3e415c93ca20a8e7979460e1347aacea60
Dhriti18 <73419650+Dhriti18@users.noreply.github.com>
2021-01-30 22:21:54 +0530

    Add files via upload

--- js/Stone.js
+++ js/Stone.js
@@ -1,31 +1,32 @@
 class  Stone{
-    constructor(x,y){
+  constructor(x,y){
+  
+  this.body=Bodies.rectangle(x,y,20,10,{isStatic:false});
+  this.width=1;
+  this.image=loadImage("images/stone.png")
+  this.height=70;
+  World.add(world, this.body);
+  
+  
+  
+  }
+  
+  display(){
+  var pos=this.body.position;
+  var angle=this.body.angle;
+    imageMode(CENTER);
+    push()
     
-    this.body=Bodies.rectangle(x,y,20,10,{isStatic:false});
-    this.width=20;
-    this.image=loadImage("images/stone.png")
-    this.height=10;
-    World.add(world, this.body);
-    
-    
-    
-    }
-    
-    display(){
-    var pos=this.body.position;
-    var angle=this.body.angle;
-      imageMode(CENTER);
-      push()
-      
-		translate(pos.x,pos.y);
-    rotate(angle);
-    fill (255,0,255);
-   
-    image(this.image, 0,20,10)
-    pop ();
-    }
+  translate(pos.x,pos.y);
+  rotate(angle);
+  fill (255,0,255);
+ 
+  image(this.image, 0,1,70);
+  pop ();
+  }
 
-  }  
+}  
+  
     
     
     
